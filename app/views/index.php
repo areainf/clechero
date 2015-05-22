e47ea1a49dae06055b9949c23638964ff2a0c33c
areainf <areainf.fce.unrc@gmail.com>
2015-05-22 19:01:14 -0300

    LandPage

--- app/views/index.php
+++ app/views/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     $url_add_dairy = Ctrl::getUrl(array('control'=>'dairy', 'action'=>'add'));
     $url_add_schema = Ctrl::getUrl(array('control'=>'schema', 'action'=>'add'));
 
@@ -8,8 +8,43 @@
 	Tambien se podria evitar esta pagina y redirigir a alguna accion por defecto
 </p>
 <div class="row">
+  <div class="col-md-6">
+    <div class="block-landpage">
+      <h1>FILIACIÓN</h1>
+      <div class="block-landpage-body">
+        <ul>
+          <li>
+            <a href="<?php echo $url_add_dairy;?>">
+              Nuevo Tambo</a>
+          </li>
+          <li class="dairies">
+            <a href="<?php echo $this->getUrlFor('dairy'); ?>">Mis Tambos</a>
+          </li>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="block-landpage">
+      <h1>DATOS PRODUCTIVOS</h1>
+      <div class="block-landpage-body">
+        <ul>
+          <li>
+            <a href="<?php echo $url_add_schema;;?>">Nuevo Control Lechero</a>
+          </li>
+          <li class="dairies">
+            <a href="<?php echo $this->getUrlFor('schema'); ?>">Listado de Controles</a>
+          </li>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+<!-- 
+<div class="row">
   <div class="col-md-4 col-md-offset-1">
-    <div class="box-owner">      
+    <div class="box-owner">
       <h3>FILIACIÓN</h3>
         <a href="<?php echo $url_add_dairy;?>" class="box-btn1 box-btn-sm">Nuevo Tambo</a><br>
         <a href="<?php echo $this->getUrlFor('dairy'); ?>" class="box-btn1 box-btn-sm">Mis Tambos</a>
@@ -26,9 +61,43 @@
     </div>
   </div>
 </div>
-<br>
-<div class="row">
-  <div class="col-md-12">
-    <div class="box-schema">REPORTES</div>
+<br> -->
+<!-- <div class="row">
+  <div class="col-xs-6 col-md-3">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Nombre del Tambo</h3>
+        <p>FECHA</p>
+        <p><a href="#" class="btn btn-primary" role="button">Análisis y Erogaciones</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-6 col-md-3">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Nombre del Tambo</h3>
+        <p>FECHA</p>
+        <p><a href="#" class="btn btn-primary" role="button">Análisis y Erogaciones</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-6 col-md-3">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Nombre del Tambo</h3>
+        <p>FECHA</p>
+        <p><a href="#" class="btn btn-primary" role="button">Análisis y Erogaciones</a></p>
+      </div>
+    </div>
+  </div>
+  <div class="col-xs-6 col-md-3">
+    <div class="thumbnail">
+      <div class="caption">
+        <h3>Nombre del Tambo</h3>
+        <p>FECHA</p>
+        <p><a href="#" class="btn btn-primary" role="button">Análisis y Erogaciones</a></p>
+      </div>
+    </div>
   </div>
 </div>
+ -->
